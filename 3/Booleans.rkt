6c8120523aa01d7afeae9d2458ba4c29b969fb93
Max New <maxsnew@gmail.com>
2013-07-17 21:52:37 -0700

    Added Boolean language reductions and tests.

--- 3/Booleans.rkt
+++ 3/Booleans.rkt
@@ -10,38 +10,94 @@
      (if t then t else t))
   (v true false))
 
-(define R
+;; Evaluation context
+;; This encodes rule "E-If"
+(define-extended-language Br
+  B
+  (p (if p then t else t)
+     hole))
+
+(define reductions
   (reduction-relation
-   B
-   (--> (if true then t_1 else t_2)
+   Br
+   (==> (if true then t_1 else t_2)
         t_1
         "E-IfTrue")
-   (--> (if false then t_1 else t_2)
+   (==> (if false then t_1 else t_2)
         t_2
-        "E-IfFalse")))
+        "E-IfFalse")
+   with
+   [(--> (in-hole p_1 a) (in-hole p_1 b))
+    (==> a b)]))
 
 ;; TESTS
 ;; Term sanity tests
 (test-equal
  (redex-match?
-  B
+  Br
   t
-  (term (if true then false else false)))
+  (term (if true
+            then false
+            else false)))
  #t)
 
 (test-equal
  (redex-match?
-  B
+  Br
   v
-  (term (if true then false else true)))
+  (term (if true
+            then false
+            else true)))
  #f)
 
 (test-equal
  (redex-match?
-  B
+  Br
   t
   (term true))
  #t)
 
-(test-results)
+;; Reduction relation tests
+(test-->>
+ reductions
+ (term true)
+ (term true))
 
+(test-->>
+ reductions
+ (term false)
+ (term false))
+
+(test-->>
+ reductions
+ (term (if true
+           then true
+           else false))
+ (term true))
+
+(test-->>
+ reductions
+ (term (if false
+           then false
+           else true))
+ (term true))
+
+(test-->>
+ reductions
+ (term (if (if true
+               then true
+               else false)
+           then true
+           else false))
+ (term true))
+
+(test-->>
+ reductions
+ (term (if true
+           then (if true
+                    then true
+                    else false)
+           else false))
+ (term true))
+
+(test-results)
