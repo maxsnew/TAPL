174254b7577ca87f8bbaf061268f6ccd7b880e97
Max New <maxsnew@gmail.com>
2013-07-21 12:07:27 -0700

    Convert back and forth from De Bruijn and explicit variables.

--- 7/Grammar.hs
+++ 7/Grammar.hs
@@ -1,8 +1,12 @@
 module Grammar where
 
 import Control.Applicative ((<$>), (<*>))
-import Control.Monad.Reader (Reader)
-import Test.QuickCheck
+import Control.Arrow ((***))
+import Control.Monad.Reader (Reader, ask, local, runReader)
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
+import Data.Set as Set (Set, empty, singleton, toList, union)
+import Test.QuickCheck (Arbitrary(..), elements, oneof, sized)
 
 -- | Untyped Lambda Calculus using De Bruijn indices for variables.
 data Term = Var Int
@@ -16,6 +20,55 @@ data NamedTerm = NVar String
                | NApp NamedTerm NamedTerm
                deriving (Show, Eq)
 
+validIdentifiers :: [String]
+validIdentifiers = (flip (:)) <$> ("":validRest) <*> validStart
+  where validStart = ['a'..'z'] ++ ['A'..'Z'] ++ "_"
+        validRest = (flip (:)) <$> ("" : validRest) <*> (validStart ++ ['0'..'9'] ++ "'")
+
+-- | Convert back and forth between De Bruijn and explicit names
+-- | Returns the nameless term and a list representing the encodings of
+-- | the free variables (the naming context).
+removeNames :: NamedTerm -> (Term, [String])
+removeNames t = withFree $ runReader (rec t) free
+  where withFree x = (x, free)
+        free = freeVars t
+        rec nt = do
+          ctx <- ask
+          case nt of
+            (NVar s)     -> let i = fromJust $ s `elemIndex` ctx in 
+              return $ Var i
+            (NAbs s bod) ->
+              Abs <$> (local (s:) $ rec bod)
+            (NApp t1 t2) ->
+              App <$> rec t1 <*> rec t2
+
+freeVars :: NamedTerm -> [String]
+freeVars t = Set.toList $ runReader (go t) []
+  where go :: NamedTerm -> Reader [String] (Set String)
+        go nt = do
+          bound <- ask
+          case nt of
+            NVar s     -> return $ if s `elem` bound
+                                     then Set.empty
+                                     else Set.singleton s
+            NAbs s bod -> local (s:) $ go bod
+            NApp t1 t2 -> do
+              free1 <- go t1
+              free2 <- go t2
+              return $ free1 `Set.union` free2
+            
+restoreNames :: Term -> [String] -> NamedTerm
+restoreNames t' ctx = runReader (rec t') (nameCtx)
+  where rec :: Term -> Reader ([String], [String]) NamedTerm
+        rec (Var i)     = do
+          (names, _) <- ask
+          return $ NVar (names !! i)
+        rec (App t1 t2) = NApp <$> rec t1 <*> rec t2
+        rec (Abs t)     = do
+          (_, unused) <- ask
+          let s = head unused in
+            NAbs s <$> (local ((s:) *** tail) $ rec t)
+        nameCtx = splitAt (length ctx) validIdentifiers
 -- | Testing
 instance Arbitrary Term where
   arbitrary = sized term'
@@ -36,15 +89,11 @@ instance Arbitrary NamedTerm where
                           , NApp <$> halved <*> halved
                           ]
             where halved = term' (n `div` 2)
-          vars = elements . map (:[]) $ ['a'..'z']                  
-
+          vars = elements . fmap (:[]) $ ['a'..'z']
   shrink (NApp t1 t2) = [t1, t2]
   shrink (NAbs _ t) = [t]
   shrink _ = []
 
--- | Named -> De Bruijn and back
-removeNames :: NamedTerm -> Reader [String] Term
-removeNames = undefined
-
-restoreNames :: Term -> Reader [String] NamedTerm
-restoreNames = undefined
+prop_idempotent_encoding :: NamedTerm -> Bool
+prop_idempotent_encoding nt = run nt == (run . run $ nt)
+  where run = uncurry restoreNames . removeNames
